c0fa72541ae8fb964fe21b85ff52c929c779a611
Joel Holdbrooks <cjholdbrooks@gmail.com>
2013-08-01 16:51:35 -0700

    Add criterium as dev dependency

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://github.com/noprompt/frak"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :profiles {:dev {:dependencies [[criterium "0.4.1"]]}} )
