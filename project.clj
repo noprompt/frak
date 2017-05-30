55eb3be6f4baed9eb0d5ba6bfcc403350d06b714
Joel Holdbrooks <cjholdbrooks@gmail.com>
2017-05-30 15:14:32 -0700

    Set clojure version to 1.8.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :jar-exclusions [#"(?:\.(?:cljx|sw[onp])|cli\.cljs?)"]
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "0.0-2156"]
                  [org.clojure/tools.cli "0.3.1"]]
   :plugins [[lein-cljsbuild "1.0.2"]
@@ -23,7 +23,7 @@
   :cljsbuild {:builds [{:id "browser"
                         :source-paths ["target/classes"]
                         :compiler {:output-to "target/js/frak.min.js"
-                                   :optimizations :advanced 
+                                   :optimizations :advanced
                                    :pretty-print false}}
                        {:id "node"
                         :source-paths ["target/classes"]
@@ -31,6 +31,6 @@
                                    :externs ["resources/externs/process.js"]
                                    :optimizations :advanced
                                    :pretty-print false
-                                   :target :nodejs}}]} 
+                                   :target :nodejs}}]}
   :main frak.cli
   :repl-options {:init-ns frak})
